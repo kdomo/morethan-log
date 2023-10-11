a9037b18b76310ba4d4deb6d6da2a5b045e2236a
도모 <dongho5088@naver.com>
2023-10-11 21:15:56 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -10,14 +10,14 @@ const CONFIG = {
     github: "kdomo",
     instagram: "d._.omo",
   },
-  /*
   projects: [
+    /*
     {
       name: `morethan-log`,
       href: "https://github.com/morethanmin/morethan-log",
     },
+    */
   ],
-  */
   // blog setting (required)
   blog: {
     title: "시행착오 과정 | 도모",
